ad0d9f0948fb075d36dc43d4707f6b3d0e48ec30
mattfwilson <mattfwilson@gmail.com>
2022-12-21 16:36:39 -0500

    Added logic for addding groups but need to fix error for conversion of str to int.

--- day1/parse_inputs.py
+++ day1/parse_inputs.py
@@ -6,13 +6,24 @@ with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
+    print(type(line))
+    # if isinstance(line, int):
+    #     print(f'Is int')    
+    # else:
+    #     print(f'Not int')
     print(line.strip())
 
 for line in lines:
+    total = 0
+    int_line = int(line)
+    print(type(int_line))
     if line.strip() == '':
-        blanks += 1
+        if total > highest:
+            highest = total
+        continue
     else:
-        filled += 1
+        int_line += total
+        continue
 
 print(f'There are {blanks} blanks lines.')
 print(f'There are {filled} filled lines.')
