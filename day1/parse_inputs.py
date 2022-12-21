adb7484664882a6c7c0f0f7045e7c99457be3fd7
mattfwilson <mattfwilson@gmail.com>
2022-12-21 02:48:52 -0500

    Correctly parsing amount of filled vs blank lines.

--- day1/parse_inputs.py
+++ day1/parse_inputs.py
@@ -1,6 +1,17 @@
-inputs = open('inputs.txt').readlines()
-for i in inputs:
-    print(i.strip())
+blanks = 0
+filled = 0
 
-# parsed = [lambda : item.strip() for item in inputs]
-# print(parsed)
+with open('inputs.txt', 'r', encoding="utf-8") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print(line.strip())
+
+for line in lines:
+    if line.strip() == '':
+        blanks += 1
+    else:
+        filled += 1
+
+print(f'There are {blanks} blanks lines.')
+print(f'There are {filled} filled lines.')
