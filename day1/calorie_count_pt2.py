fc66640974855a979809b4b9b1baa366c32ab7de
mattfwilson <mattfwilson@gmail.com>
2022-12-23 03:43:41 -0500

    AoC day 1 part 2 complete

--- day1/calorie_count_pt2.py
+++ day1/calorie_count_pt2.py
@@ -1,5 +1,6 @@
 highest = 0
 total = 0
+cal_lst = []
 
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
@@ -8,15 +9,18 @@ for line in lines:
     line = line.strip()
     if line == '':
         print(f'EMPTY LINE - {type(line)}')
-        if total > highest:
-            highest = total
-            total = 0
-        else:
-            total = 0
+        cal_lst.append(total)
+        total = 0
     if line != '':
         line = int(line)
         print(f'{line} - {type(line)}')
         total += line
             
-print(f'Current Group Total: {total}')
-print(f'Highest Calories: {highest}')
+cal_lst.sort(reverse=True)
+print(cal_lst)
+top_three = 0
+
+for i in cal_lst[:3]:
+    top_three += i
+print(top_three)
+
