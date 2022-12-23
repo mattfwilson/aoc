347d1348737cc3bc46fa8e2c5c090d6d2211328f
mattfwilson <mattfwilson@gmail.com>
2022-12-22 20:44:22 -0500

    Fixed error causing lines not totaling at all. Current issue:  totals are compounding and not resetting properly.

--- day1/calorie_count.py
+++ day1/calorie_count.py
@@ -5,17 +5,17 @@ with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
-    line = int(float(line.strip()))
-    print(line)
-    print(type(line))
-    if line == '': # if line is empty
-        if total > highest: 
-            highest = total # replace highest with new total
+    line = line.strip()
+    if line == '':
+        print(f'EMPTY LINE - {type(line)}')
+        if total > highest:
+            highest = total
         else:
-            continue
-    else: # if line has a value
-        line += total
-    continue
-
-print(f'Current Total: {total}')
+            total = 0
+    if line != '':
+        line = int(line)
+        print(f'{line} - {type(line)}')
+        total += line
+            
+print(f'Current Group Total: {total}')
 print(f'Highest Calories: {highest}')
