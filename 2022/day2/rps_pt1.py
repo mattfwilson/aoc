8a53d11e9b7dcb005ad347481a24522a91e76d02
mattfwilson <mattfwilson@gmail.com>
2022-12-24 01:06:51 -0500

    First attempt at translating lettered results to numerical outputs

--- 2022/day2/rps_pt1.py
+++ 2022/day2/rps_pt1.py
@@ -1,7 +1,13 @@
 scores = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+numbered = []
 
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
-    print(line.strip())
+    print(line.strip().replace(' ', ''))
+    for key, val in scores.items():
+        for char in line:
+            if key in char:
+                res = numbered.append(val)
+                print(res)
