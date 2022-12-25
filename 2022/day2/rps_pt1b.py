16f96b58b9ff9a50cbf769426e3cccd557955ed1
mattfwilson <mattfwilson@gmail.com>
2022-12-24 22:32:16 -0500

    Trying to figure out why VSCode won't run (blue dot in terminal)

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,15 +1,9 @@
-scores = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
-
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
     for char in line:
         char.replace('A', '1')
-        char.replace('X', '1')
-        char.replace('B', '2')
-        char.replace('Y', '2')
-        char.replace('C', '3')
-        char.replace('Z', '3')
+    
     print(line.strip().replace(' ', ''))
 
