dff116d71411d9eff98b741d10858e3d8ddb71e5
mattfwilson <mattfwilson@gmail.com>
2022-12-26 01:06:14 -0500

    Part 1 of day 1 2021  complete

--- 2021/day1/sonar_pt1.py
+++ 2021/day1/sonar_pt1.py
@@ -1,6 +1,19 @@
+increases = 0
+decreases = 0
+current = 157
 
 with open('inputs.txt', 'r') as file:
     depths = file.readlines()
 
 for depth in depths:
-    print(depth.strip())
+    num_depth = int(depth.strip())
+    print(f'{num_depth} - {type(num_depth)}')
+    if num_depth > current:
+        current = num_depth
+        increases += 1
+    else:
+        current = num_depth
+        decreases += 1
+
+print(f'Increases: {increases}')
+print(f'Decreases: {decreases}')
