55fdb762f1bc90f08952b64472ac14549882566e
mattfwilson <mattfwilson@gmail.com>
2022-12-25 23:40:52 -0500

    Simplifying script to debug running issue

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,20 +1,8 @@
+
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
-    for char in line:
-        if char == 'A':
-            char.replace('A', '1')
-        if char == 'B':
-            char.replace('A', '2')
-        if char == 'C':
-            char.replace('A', '3')
-        if char == 'X':
-            char.replace('A', '1')
-        if char == 'Y':
-            char.replace('A', '2')
-        if char == 'Z':
-            char.replace('A', '3')
-    
     print(line.strip().replace(' ', ''))
-
+    for char in line:
+        print(line)
