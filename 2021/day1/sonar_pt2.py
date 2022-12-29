2c34711899f1007220367519f19c1611e568b869
mattfwilson <mattfwilson@gmail.com>
2022-12-28 21:42:05 -0500

    Adding first segment properly but re-looping through and multiplying outcomes together instead of the segmenting being linear

--- 2021/day1/sonar_pt2.py
+++ 2021/day1/sonar_pt2.py
@@ -1,19 +1,20 @@
 counter = 0
 groups = []
+segment = 0
 
 with open('inputs.txt', 'r') as file:
     depths = file.readlines()
 
 while True:
     for depth in depths:
-        three = 0
         num_depth = int(depth.strip())
-        three += num_depth
-        print(f'{num_depth} - {type(num_depth)}')
-        counter += 1
-        if counter == 20:
-            groups.append(three)
+        if counter == 3:
+            groups.append(segment)
             counter = 0
             threes = 0
+        else:
+            segment += num_depth # first segment should be 482
+            print(f'{num_depth} - {type(num_depth)}')
+            counter += 1
     break
 print(groups)
