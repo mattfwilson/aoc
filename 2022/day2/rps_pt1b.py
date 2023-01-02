9a02993988a222c8a35510c138578cb9ff99065f
mattfwilson2 <mattfwilson+1@gmail.com>
2023-01-02 04:15:22 -0500

    Re-added inputs and stripped spaces from them

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,10 +1,5 @@
-
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
     print(line.strip().replace(' ', ''))
-    for char in line:
-        print(line)
-
-print(f'Hello world')
