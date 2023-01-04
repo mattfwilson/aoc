84ea6978d45c1a583ce4000537e3950fd29571d6
mattfwilson <mattfwilson@gmail.com>
2023-01-03 23:03:13 -0500

    Created main list from inputs and list of groups of three inputs

--- 2021/day1/sonar_pt2.py
+++ 2021/day1/sonar_pt2.py
@@ -1,16 +1,18 @@
-counter = 1
-segment = 0
-groups = []
+input_lst = []
+grouped_lst = []
 
 with open('inputs.txt', 'r') as file:
     depths = file.readlines()
 
 while True:
+    counter = 1
+    segment = 0
     for depth in depths:
         num_depth = int(depth.strip())
-        segment = num_depth[counter-1] + num_depth + num_depth[counter+1]
-        groups.append(segment)
+        input_lst.append(num_depth)
+        segment = input_lst[counter-1] + input_lst[counter] + input_lst[counter+1]
+        grouped_lst.append(segment)
         counter += 1
     break
 
-print(groups)
+print(grouped_lst)
