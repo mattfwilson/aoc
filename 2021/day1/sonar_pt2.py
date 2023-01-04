c3c3e5941f2d9725938cb97c4f36e270a31b0cce
mattfwilson <mattfwilson@gmail.com>
2023-01-04 15:17:11 -0500

    Figured out main logic for calculating grouped increases/decreases but need to find issue with calculating last evaluations when IndexError occurs in list

--- 2021/day1/sonar_pt2.py
+++ 2021/day1/sonar_pt2.py
@@ -1,18 +1,41 @@
 input_lst = []
-grouped_lst = []
+segment_lst = []
+count = 0
+increases = 0
+decreases = 0
+current = 157
 
 with open('inputs.txt', 'r') as file:
     depths = file.readlines()
 
-while True:
-    count = 1
-    segment = 0
-    for depth in depths:
-        int_depth = int(depth.strip())
-        input_lst.append(int_depth)
-        segment = int_depth[count-1] + int_depth[count] + int_depth[count+1]
-        grouped_lst.append(segment)
-        count += 1
-    break
+for line in depths:
+    input_lst.append(int(line.strip()))
+print(input_lst)
 
-print(grouped_lst)
+for depth in input_lst:
+    try:
+        segment = 0
+        segment = input_lst[count] + input_lst[count+1] + input_lst[count+2]
+        print(input_lst.index(depth))
+        print(segment)
+        count += 1
+        segment_lst.append(segment)
+    except IndexError:
+        pass
+    finally:
+        if segment > current:
+            current = segment
+            increases += 1
+            print(f'{segment} increased!')
+        else:
+            current = segment
+            decreases += 1
+            print(f'{segment} decreased!')
+
+print(f'Increases: {increases}')
+print(f'Decreases: {decreases}') 
+        
+
+
+
+
