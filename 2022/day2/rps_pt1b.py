0aa592ec125119b0ce36a3b651ed5fd1ffc98587
mattfwilson <mattfwilson@gmail.com>
2023-01-06 02:03:05 -0500

    Main logic (not properly working) for how to solve day 2 part 1

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,11 +1,18 @@
+split_lst = []
+
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
-    print(line.strip().replace(' ', ''))
-    for char in line:
-        if char == 'A':
-            print(f'There is an A.')
+    split_lst.append(line.strip().split(' '))
 
+for item in split_lst:
+    print(item)
+    for char in lst:
+        if char == 'A' or char == 'X':
+            split_lst.replace(char, 1)
+        if char == 'B' or char == 'Y':
+            split_lst.replace(char, 2)
+        if char == 'C' or char == 'Z':
+            split_lst.replace(char, 3)
 
-# Testing
