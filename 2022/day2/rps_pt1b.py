ce9254c9134057a53e3171d9e1534834a7df619b
mattfwilson <mattfwilson@gmail.com>
2023-01-09 02:47:59 -0500

    Main modular math calculating player wins and losses

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,5 +1,7 @@
 split_lst = []
 count = 0 
+wins = 0
+losses = 0
 
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
@@ -23,4 +25,15 @@ for item in split_lst:
     if item[1] == 'Z':
         item[1] = 3
 
+    if (item[0] + 1) % 3 == item[1]:
+        print(f'You lost.')
+        losses += 1
+    elif (item[1] + 1) % 3 == item[0]:
+        print(f'You won!')
+        wins += 1
+    else:
+        print('It was a tie.')
+
 print(split_lst)
+print(f'Wins: {wins}')
+print(f'Losses: {losses}')
