ba86877bd7544f5467031be0714c5c92aaeff5db
mattfwilson <mattfwilson@gmail.com>
2023-01-10 02:14:05 -0500

    First attempt at calculating wins for day 1, part 1

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -2,6 +2,9 @@ split_lst = []
 count = 0 
 wins = 0
 losses = 0
+rock = 0
+paper = 0
+scissors = 0
 
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
@@ -30,9 +33,11 @@ for item in split_lst:
         losses += 1
     elif (item[1] + 1) % 3 == item[0]:
         print(f'You won!')
-        wins += 1
+        wins += 6
+        wins += item[1]
     else:
         print('It was a tie.')
+        wins += 3
 
 print(split_lst)
 print(f'Wins: {wins}')
