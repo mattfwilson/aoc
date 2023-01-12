700f5d83d14c4355d81414d643ba6099a1ce2dc7
mattfwilson <mattfwilson@gmail.com>
2023-01-11 20:57:28 -0500

    Check on modulus math working properly

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,7 +1,7 @@
 split_lst = []
 count = 0 
-wins = 0
-losses = 0
+player_wins = 0
+comp_wins = 0
 rock = 0
 paper = 0
 scissors = 0
@@ -35,19 +35,19 @@ for item in split_lst:
         scissors += 1
 
     if (item[0] + 1) % 3 == item[1]:
-        print(f'You lost.')
-        losses += 1
+        print(f'Computer wins.')
+        comp_wins += 1
     elif (item[1] + 1) % 3 == item[0]:
         print(f'You won!')
-        wins += 6
-        wins += item[1]
+        player_wins += 6
+        player_wins += item[1]
     else:
         print('It was a tie.')
-        wins += 3
+        player_wins += 3
 
 print(split_lst)
-print(f'\nWins: {wins}')
-print(f'Losses: {losses}\n')
-print(f'Rock: {rock}')
-print(f'Paper: {paper}')
-print(f'Scissors: {scissors}\n')
+print(f'\nPlayer Wins: {player_wins}')
+print(f'Computer Wins: {comp_wins}\n')
+print(f'Rocks played: {rock}')
+print(f'Papers played: {paper}')
+print(f'Scissors played: {scissors}\n')
