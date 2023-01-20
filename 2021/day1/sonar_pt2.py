d6d791a8cba2a6c29a9d773c3d8827c0199014cd
mattfwilson <mattfwilson@gmail.com>
2023-01-20 06:12:15 -0500

    Removed emojis

--- 2021/day1/sonar_pt2.py
+++ 2021/day1/sonar_pt2.py
@@ -1,8 +1,6 @@
-import emoji
-
 input_lst = []
 segment_lst = []
-count = 0
+counter = 0
 increases = 0
 current = 157
 
@@ -13,22 +11,22 @@ for depth in depths:
     input_lst.append(int(depth.strip()))
     try:
         total_seg = 0
-        total_seg = input_lst[count] + input_lst[count + 1] + input_lst[count + 2]
-        count += 1
+        total_seg = input_lst[counter] + input_lst[counter + 1] + input_lst[counter + 2]
+        counter += 1
         segment_lst.append(total_seg)
     except IndexError:
-        print('Hit IndexError (cannot sum indexes further')
+        print('Hit IndexError (cannot sum next three indexes')
 
 for index, segment in enumerate(segment_lst):
     if segment > current:
         current = segment
         increases += 1
-        print(emoji.emojize(f'Index: {index} - {segment} :chart_increasing:'))
+        print(f'Index: {index} - {segment}')
     if segment <= current:
         current = segment
-        print(emoji.emojize(f'Index: {index} - {segment} :chart_decreasing:'))
+        print(f'Index: {index} - {segment}')
 
-print(f'\nCounter: {count}')
+print(f'\nCounter: {counter}')
 print(f'Increases: {increases}')
         
 
