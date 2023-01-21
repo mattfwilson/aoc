19eed3181bef59a8d6538a70744053294fc8b952
mattfwilson <mattfwilson@gmail.com>
2023-01-21 03:49:30 -0500

    Renamed inputs list and cleaned up syntax

--- 2022/day2/rps_pt1b.py
+++ 2022/day2/rps_pt1b.py
@@ -1,20 +1,20 @@
-results = []
+inputs = []
 player_score = 0
 shape_points = 0
 
 with open('inputs.txt', 'r') as file:
     lines = file.readlines()
+    for line in lines:
+        inputs.append(line.strip().split(' '))
 
-for line in lines:
-    results.append(line.strip().split(' '))
-
-for item in results:
+for item in inputs:
     if item[0] == 'A':
         item[0] = 1
     if item[0] == 'B':
         item[0] = 2
     if item[0] == 'C':
         item[0] = 3
+        
     if item[1] == 'X':
         item[1] = 1
     if item[1] == 'Y':
@@ -22,19 +22,19 @@ for item in results:
     if item[1] == 'Z':
         item[1] = 3
 
-for item in results:
+for item in inputs:
     if (item[0] + 1) % 3 == item[1]:
         player_score += item[1]
-        print(f'Computer wins. Score +{item[1]}')
+        print(f'Computer wins. (+{item[1]})')
     elif (item[1] + 1) % 3 == item[0]:
         player_score += 6
         player_score += item[1]
         total_score = 6 + item[1]
-        print(f'Player wins. Score +{total_score}')
+        print(f'Player wins. (+{total_score})')
     else:
         player_score += 3
         player_score += item[1]
         total_score = 3 + item[1]
-        print(f'Tie. Score +{total_score}')
+        print(f'Tie game. (+{total_score})')
 
 print(f'Player Score: {player_score}')
