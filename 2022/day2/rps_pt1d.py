b6453687e46015bca4c034db252ef7263bf8c5fa
mattfwilson <mattfwilson@gmail.com>
2023-05-28 05:55:42 -0400

    Deleted unnecessary list

--- 2022/day2/rps_pt1d.py
+++ 2022/day2/rps_pt1d.py
@@ -1,32 +1,29 @@
 import numpy as np
 import sys
 
-inputs = []
 player_score = 0
-shape_points = 0
-line_count = 0
-item_count = 0
 
-inputs = np.loadtxt('inputs.csv', delimiter=' ', dtype=str)
-inputs[(inputs == 'A') | (inputs == 'X')] = 1
-inputs[(inputs == 'B') | (inputs == 'Y')] = 2
-inputs[(inputs == 'C') | (inputs == 'Z')] = 3
+str_inputs = np.loadtxt('inputs.csv', delimiter=' ', dtype=str)
 np.set_printoptions(threshold=sys.maxsize)
-int_inputs = np.int_(inputs)
+
+str_inputs[(str_inputs == 'A') | (str_inputs == 'X')] = 1
+str_inputs[(str_inputs == 'B') | (str_inputs == 'Y')] = 2
+str_inputs[(str_inputs == 'C') | (str_inputs == 'Z')] = 3
+int_inputs = np.int_(str_inputs)
 
 for item in int_inputs:
     if (item[0] + 1) % 3 == item[1]:
         player_score += item[1]
-        print(f'Computer wins. (+{item[1]})')
+        print(f'{item} Computer wins. (+{item[1]})')
     elif (item[1] + 1) % 3 == item[0]:
         player_score += 6
         player_score += item[1]
         total_score = 6 + item[1]
-        print(f'Player wins. (+{total_score})')
-    else:
+        print(f'{item} Player wins. (+{total_score})')
+    elif item[0] == item[1]:
         player_score += 3
         player_score += item[1]
         total_score = 3 + item[1]
-        print(f'Tie game. (+{total_score})')
+        print(f'{item} Tie game. (+{total_score})')
 
 print(f'Player Score: {player_score}')
