6d65d41a7e283d09b76508977bb34c479675ad20
mattfwilson <mattfwilson@gmail.com>
2023-05-27 21:17:45 -0400

    Successfully converted all items in numpy array to int

--- 2022/day2/rps_pt1d.py
+++ 2022/day2/rps_pt1d.py
@@ -15,28 +15,26 @@ for line in inputs:
 print(f'Line count: {line_count}')
 print(f'Item_count: {item_count}')
 
-A_convert = inputs[inputs == 'A'] = 1
-B_convert = inputs[inputs == 'B'] = 2
-C_convert = inputs[inputs == 'C'] = 3
-X_convert = inputs[inputs == 'X'] = 1
-Y_convert = inputs[inputs == 'Y'] = 2
-Z_convert = inputs[inputs == 'Z'] = 3
+inputs[(inputs == 'A') | (inputs == 'X')] = 1
+inputs[(inputs == 'B') | (inputs == 'Y')] = 2
+inputs[(inputs == 'C') | (inputs == 'Z')] = 3
 np.set_printoptions(threshold=sys.maxsize)
-print(inputs)
 
-# for item in inputs:
-#     if (item[0] + 1) % 3 == item[1]:
-#         player_score += item[1]
-#         print(f'Computer wins. (+{item[1]})')
-#     elif (item[1] + 1) % 3 == item[0]:
-#         player_score += 6
-#         player_score += item[1]
-#         total_score = 6 + item[1]
-#         print(f'Player wins. (+{total_score})')
-#     else:
-#         player_score += 3
-#         player_score += item[1]
-#         total_score = 3 + item[1]
-#         print(f'Tie game. (+{total_score})')
+int_inputs = np.int_(inputs)
 
-# print(f'Player Score: {player_score}')
+for item in int_inputs:
+    if (item[0] + 1) % 3 == item[1]:
+        player_score += item[1]
+        print(f'Computer wins. (+{item[1]})')
+    elif (item[1] + 1) % 3 == item[0]:
+        player_score += 6
+        player_score += item[1]
+        total_score = 6 + item[1]
+        print(f'Player wins. (+{total_score})')
+    else:
+        player_score += 3
+        player_score += item[1]
+        total_score = 3 + item[1]
+        print(f'Tie game. (+{total_score})')
+
+print(f'Player Score: {player_score}')
