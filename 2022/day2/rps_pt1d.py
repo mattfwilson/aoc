c56991ecae1dd574945f42c10887350f975e8e89
mattfwilson <mattfwilson@gmail.com>
2023-05-29 03:10:06 -0400

    Correctly adding you shape points instead of the opponents

--- 2022/day2/rps_pt1d.py
+++ 2022/day2/rps_pt1d.py
@@ -11,19 +11,19 @@ str_inputs[(str_inputs == 'B') | (str_inputs == 'Y')] = 2
 str_inputs[(str_inputs == 'C') | (str_inputs == 'Z')] = 3
 int_inputs = np.int_(str_inputs)
 
-for item in int_inputs:
-    if (item[0] + 1) % 3 == item[1]:
-        player_score += item[1]
-        print(f'{item} Computer wins. (+{item[1]})')
-    elif (item[1] + 1) % 3 == item[0]:
+for shape in int_inputs:
+    if (shape[0] + 1) % 3 == shape[1]: # you lose
+        player_score += shape[0]
+        print(f'{shape} Computer wins. (+{shape[0]})')
+    elif (shape[1] + 1) % 3 == shape[0]: # you win
         player_score += 6
-        player_score += item[1]
-        total_score = 6 + item[1]
-        print(f'{item} Player wins. (+{total_score})')
-    elif item[0] == item[1]:
+        player_score += shape[0]
+        total_score = 6 + shape[0]
+        print(f'{shape} Player wins. (+{total_score})')
+    elif shape[0] == shape[1]: # you tie
         player_score += 3
-        player_score += item[1]
-        total_score = 3 + item[1]
-        print(f'{item} Tie game. (+{total_score})')
+        player_score += shape[0]
+        total_score = 3 + shape[0]
+        print(f'{shape} Tie game. (+{total_score})')
 
 print(f'Player Score: {player_score}')
