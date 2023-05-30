5db22af5cef3becd55d1a5801139c4a543a3b70d
mattfwilson <mattfwilson@gmail.com>
2023-05-30 00:51:36 -0400

    Still debugging modular math for counting wins

--- 2022/day2/rps_pt1d.py
+++ 2022/day2/rps_pt1d.py
@@ -11,19 +11,19 @@ str_inputs[(str_inputs == 'B') | (str_inputs == 'Y')] = 2
 str_inputs[(str_inputs == 'C') | (str_inputs == 'Z')] = 3
 int_inputs = np.int_(str_inputs)
 
-for shape in int_inputs:
-    if (shape[0] + 1) % 3 == shape[1]: # you lose
-        player_score += shape[0]
-        print(f'{shape} Computer wins. (+{shape[0]})')
-    elif (shape[1] + 1) % 3 == shape[0]: # you win
+for throw in int_inputs:
+    if (throw[0] + 1) % 3 == throw[1]: # you lose
+        player_score += throw[0]
+        print(f'{throw} Computer wins. (+{throw[0]})')
+    elif (throw[1] + 1) % 3 == throw[0]: # you win
         player_score += 6
-        player_score += shape[0]
-        total_score = 6 + shape[0]
-        print(f'{shape} Player wins. (+{total_score})')
-    elif shape[0] == shape[1]: # you tie
+        player_score += throw[0]
+        total_score = 6 + throw[0]
+        print(f'{throw} Player wins. (+{total_score})')
+    elif throw[0] == throw[1]: # you tie
         player_score += 3
-        player_score += shape[0]
-        total_score = 3 + shape[0]
-        print(f'{shape} Tie game. (+{total_score})')
+        player_score += throw[0]
+        total_score = 3 + throw[0]
+        print(f'{throw} Tie game. (+{total_score})')
 
 print(f'Player Score: {player_score}')
