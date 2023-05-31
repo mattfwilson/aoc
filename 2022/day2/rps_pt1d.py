0cd6eaec0179bbf0a0e26bb0e6acf0f97de133ed
mattfwilson <mattfwilson@gmail.com>
2023-05-31 09:49:53 -0400

    Removed line breaks

--- 2022/day2/rps_pt1d.py
+++ 2022/day2/rps_pt1d.py
@@ -2,9 +2,9 @@ map_input = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X': 'Rock', 'Y': 'Pape
 shape_pts = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
 game_pts = {'Lose': 0, 'Draw': 3, 'Win': 6}
 
-with open('inputs.txt', 'r') as f:
-    lines = f.readlines()
-    rounds = [entry.strip(' ').replace(' ', '') for entry in lines]
+with open('inputs.txt', 'r') as file:
+    lines = file.readlines()
+    rounds = [entry.strip(' ').replace(' ', '').replace('\n', '') for entry in lines]
 
 def points_per_round(throw):
     opp_shape = map_input[throw[0]]
