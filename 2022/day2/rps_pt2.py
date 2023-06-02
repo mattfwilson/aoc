10cf0f53dbed29585f6288fee27185825600c323
mattfwilson <mattfwilson@gmail.com>
2023-06-01 21:16:06 -0400

    More editing with vim

--- 2022/day2/rps_pt2.py
+++ 2022/day2/rps_pt2.py
@@ -6,27 +6,28 @@ with open('inputs.txt', 'r') as file:
     lines = file.readlines()
     rounds = [entry.strip(' ').replace(' ', '').replace('\n', '') for entry in lines]
 
-def calc_outcome(opp_shape):    
-    if opp_shape == 'Rock':
-        pass
-    elif opp_shape == 'Scissors':
-        pass
-    else: 
-        pass
+def calc_win(our_shape):    
+    return int(shape_pts.get(our_shape) + 6)
 
+def calc_draw(our_shape):
+    return int(shape_pts.get(our_shape) + 3)
+
+def calc_loss(our_shape):
+    return int(shape_pts.get(our_shape))
+    
 def points_per_round(throw):
     opp_shape = map_input[throw[0]]
     our_shape = map_input[throw[1]]
     print(throw)
        
-    if our_shape == 'X': # need to lose
-        calc_outcome()
-    elif our_shape == 'Y': # need to draw
-        calc_outcome()
-    else: # need to win
-        calc_outcome()
+    if our_shape == 'X':
+        calc_win(our_shape)
+    elif our_shape == 'Y':
+        calc_draw(our_shape)
+    else:
+        calc_loss(our_shape)
 
-total = 0
+score = 0
 for throw in rounds:
-    total += points_per_round(throw)
-print(total)
+    score += points_per_round(throw)
+print(score)
