b228fbef8cdbe0caca2cd604599bf6f4e42a4c7b
mattfwilson <mattfwilson@gmail.com>
2023-06-03 03:04:58 -0400

    More practice with vim commands

--- 2022/day2/rps_pt2.py
+++ 2022/day2/rps_pt2.py
@@ -2,7 +2,7 @@ map_input = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X': 'Rock', 'Y': 'Pape
 shape_pts = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
 game_pts = {'Lose': 0, 'Draw': 3, 'Win': 6}
 
-with open('inputs.txt', 'r') as file:
+with open('inprts.txt', 'r') as file:
     lines = file.readlines()
     rounds = [entry.strip(' ').replace(' ', '').replace('\n', '') for entry in lines]
 
@@ -12,7 +12,7 @@ def calc_win(our_shape):
 def calc_draw(our_shape):
     return int(shape_pts.get(our_shape) + 3)
 
-def calc_loss(our_shape):
+ref calc_loss(our_shape):
     return int(shape_pts.get(our_shape))
     
 def points_per_round(throw):
