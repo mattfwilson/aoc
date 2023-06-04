03298ec197b10dc1dd2f4561c66a3345780cbc12
mattfwilson <mattfwilson@gmail.com>
2023-06-04 17:01:04 -0400

    Functions calculating total but still not accurately

--- 2022/day2/rps_pt2.py
+++ 2022/day2/rps_pt2.py
@@ -1,6 +1,6 @@
-map_input = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X': 'Rock', 'Y': 'Paper', 'Z': 'Scissors'}
-shape_pts = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
-game_pts = {'Lose': 0, 'Draw': 3, 'Win': 6} 
+map_input = {'A': 'rock', 'B': 'paper', 'C': 'scissors', 'X': 'rock', 'Y': 'paper', 'Z': 'scissors'}
+shape_pts = {'rock': 1, 'paper': 2, 'scissors': 3}
+game_pts = {'lose': 0, 'draw': 3, 'win': 6} 
 score = 0
 
 with open('inputs.txt', 'r') as file:
@@ -8,46 +8,40 @@ with open('inputs.txt', 'r') as file:
     rounds = [entry.strip(' ').replace(' ', '').replace('\n', '') for entry in lines]
 
 def calc_win(opp_shape):    
-    print(opp_shape)
-    if opp_shape == 'Rock':
-        return shape_pts.get('Paper') + 6
-    elif opp_shape == 'Paper':
-        return shape_pts.get('Scissors') + 6
-    elif opp_shape == 'Scissors':
-        return shape_pts.get('Rock') + 6
+    if opp_shape == 'rock':
+        return shape_pts.get('paper') + 6
+    elif opp_shape == 'paper':
+        return shape_pts.get('scissors') + 6
+    elif opp_shape == 'scissors':
+        return shape_pts.get('rock') + 6
 
 def calc_draw(opp_shape):
-    print(opp_shape)
-    if opp_shape == 'Rock':
-        return shape_pts.get('Rock') + 1
-    elif opp_shape == 'Paper':
-        return shape_pts.get('Paper') + 1
-    elif opp_shape == 'Scissors':
-        return shape_pts.get('Scissors') + 1
+    if opp_shape == 'rock':
+        return shape_pts.get('rock') + 1
+    elif opp_shape == 'paper':
+        return shape_pts.get('paper') + 1
+    elif opp_shape == 'scissors':
+        return shape_pts.get('scissors') + 1
 
 def calc_loss(opp_shape):
-    print(opp_shape)
-    if opp_shape == 'Rock':
-        return shape_pts.get('Scissors')
-    elif opp_shape == 'Paper':
-        return shape_pts.get('Rock')
-    elif opp_shape == 'Scissors':
-        return shape_pts.get('Paper')
+    if opp_shape == 'rock':
+        return shape_pts.get('scissors')
+    elif opp_shape == 'paper':
+        return shape_pts.get('rock')
+    elif opp_shape == 'scissors':
+        return shape_pts.get('paper')
 
-def points_per_round(throw):
-    opp_shape = map_input[throw[0]]
-    our_shape = map_input[throw[1]]
-    
+def points_per_round(shapes, score):
+    opp_shape = map_input[shapes[0]]
+    our_shape = map_input[shapes[1]]
+    print(f'{opp_shape} vs {our_shape}') 
     if our_shape == 'X':
-        calc_win(opp_shape)
+        return calc_loss(opp_shape)
     elif our_shape == 'Y':
-        calc_draw(opp_shape)
+        return calc_draw(opp_shape)
     else:
-        calc_loss(opp_shape)
+        return calc_win(opp_shape)
 
-print('Before loop')
-for throw in rounds:
-    score = sum(points_per_round(throw))
-print(score)
-
-print('End of file, lol')
+for shapes in rounds:
+    score += points_per_round(shapes, score)
+print(f'Your score: {score}')
