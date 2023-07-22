4d6f57793e9b96c8e895678796e2d009e307bda4
mattfwilson <mattfwilson@gmail.com>
2023-07-21 23:55:57 -0400

    Simplified names

--- 2021/day1/sonar_pt2.py
+++ 2021/day1/sonar_pt2.py
@@ -1,5 +1,5 @@
-input_lst = []
-segment_lst = []
+inputs = []
+segments = []
 counter = 0
 increases = 0
 current = 157
@@ -8,15 +8,15 @@ with open('inputs.txt', 'r') as file:
     depths = file.readlines()
 
 for index, depth in enumerate(depths):
-    input_lst.append(int(depth.strip()))
+    inputs.append(int(depth.strip()))
     try:
-        total_seg = input_lst[counter] + input_lst[counter + 1] + input_lst[counter + 2]
-        segment_lst.append(total_seg)
+        total_seg = inputs[counter] + inputs[counter + 1] + inputs[counter + 2]
+        segments.append(total_seg)
         counter += 1
     except IndexError:
         print('Hit IndexError (cannot sum next three indexes)')
 
-for index, segment in enumerate(segment_lst):
+for index, segment in enumerate(segments):
     if segment > current:
         current = segment
         increases += 1
