00228d8e31b897d01db3e0ffa4f90fa4f4846614
mattfwilson <mattfwilson@gmail.com>
2023-07-26 16:32:44 -0400

    Cleaning up inputs into a dict

--- 2021/day1/sonar_pt2b.py
+++ 2021/day1/sonar_pt2b.py
@@ -3,12 +3,6 @@ with open('inputs.txt', 'r') as file:
 
 print(depths)
 
-#for x in depths:
-#    if isinstance(x, int) == False:
-#        print('This is not an INT!')
-#    else:
-#        print('IS INT')
-
 def calc_increases():
     segments = [sum(seg_tup) for seg_tup in zip(depths, depths[1:], depths[2:])]
     result = [sum1 < sum2 for sum1, sum2 in zip(segments, segments[1:])] 
--- 2021/day2/pos_pt1.py
+++ 2021/day2/pos_pt1.py
@@ -1,2 +1,7 @@
 with open('inputs.txt', 'r') as file:
-    directions = [int(item) for item in file.read().strip().split('\n')]
+    lines = file.readlines()
+    print(lines)
+    stripped = [item.replace('\n', '') for item in lines]
+    print(stripped)
+    data_dict = {key_value.split(' ')[0]: key_value.split(' ')[1] for key_value in stripped}
+    print(data_dict)
