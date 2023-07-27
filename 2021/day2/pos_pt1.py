59802f8de3202b54c1be12b7fdec1a0c379ce28a
mattfwilson <mattfwilson@gmail.com>
2023-07-26 23:34:30 -0400

    Still trying to convert entire list to dict...

--- 2021/day2/pos_pt1.py
+++ 2021/day2/pos_pt1.py
@@ -1,7 +1,40 @@
 with open('inputs.txt', 'r') as file:
-    lines = file.readlines()
-    print(lines)
-    stripped = [item.replace('\n', '') for item in lines]
-    print(stripped)
-    data_dict = {key_value.split(' ')[0]: key_value.split(' ')[1] for key_value in stripped}
-    print(data_dict)
+    imported = file.readlines()
+    lst_inputs = [line.replace('\n', '').split(' ') for line in imported]
+    dict_inputs = {key: value for key, value in lst_inputs}
+    dict_inputs2 = dict(lst_inputs)
+    print(dict_inputs)
+    print(dict_inputs2)
+    counter = 0
+    for key_val in lst_inputs:
+        counter += 1
+        dict_inputs[key_val[0]] = key_val[1]
+        print(key_val)
+    print(counter)
+    print(dict_inputs)
+
+#for pair in lst_inputs:
+#    pair[1] = int(pair[1])
+#
+#print(type(lst_inputs[0][0]))
+#print(type(lst_inputs[0][1]))
+#
+#forwards = 0
+#ups = 0
+#downs = 0
+#
+#def move_sub(direction):
+#    if direction[0] == 'forward':
+#        return forwards += direction[1]
+#    elif direction[0] == 'up':
+#        return ups += direction[1]
+#    else:
+#        return downs -= direction[1]
+#
+#for action in lst_inputs:
+#    print(type(action[0]))
+#    print(type(action[1]))
+#
+#print(forwards)
+#print(ups)
+#print(downs)
