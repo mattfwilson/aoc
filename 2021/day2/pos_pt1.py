ab6f2a1ebc82423652bdcd5945cf15506d3a8936
mattfwilson <mattfwilson@gmail.com>
2023-07-27 23:02:59 -0400

    Func totaling directions properly albeit incorrectly

--- 2021/day2/pos_pt1.py
+++ 2021/day2/pos_pt1.py
@@ -1,40 +1,31 @@
 with open('inputs.txt', 'r') as file:
     imported = file.readlines()
     lst_inputs = [line.replace('\n', '').split(' ') for line in imported]
-    dict_inputs = {key: value for key, value in lst_inputs}
-    dict_inputs2 = dict(lst_inputs)
-    print(dict_inputs)
-    print(dict_inputs2)
-    counter = 0
-    for key_val in lst_inputs:
-        counter += 1
-        dict_inputs[key_val[0]] = key_val[1]
-        print(key_val)
-    print(counter)
-    print(dict_inputs)
 
-#for pair in lst_inputs:
-#    pair[1] = int(pair[1])
-#
-#print(type(lst_inputs[0][0]))
-#print(type(lst_inputs[0][1]))
-#
-#forwards = 0
-#ups = 0
-#downs = 0
-#
-#def move_sub(direction):
-#    if direction[0] == 'forward':
-#        return forwards += direction[1]
-#    elif direction[0] == 'up':
-#        return ups += direction[1]
-#    else:
-#        return downs -= direction[1]
-#
-#for action in lst_inputs:
-#    print(type(action[0]))
-#    print(type(action[1]))
-#
-#print(forwards)
-#print(ups)
-#print(downs)
+for pair in lst_inputs:
+    pair[1] = int(pair[1])
+
+forwards = 0
+ups_downs = 0
+
+def move_sub(direction):
+    global forwards
+    global ups_downs
+    if direction[0] == 'forward':
+        forwards += direction[1]
+        return forwards
+    elif direction[0] == 'up':
+        ups_downs += direction[1]
+        return ups_downs
+    else:
+        ups_downs -= direction[1]
+        return ups_downs
+
+for i in lst_inputs:
+    move_sub(i)
+    
+print(forwards)
+print(ups_downs)
+
+position = forwards * ups_downs
+print(position)
