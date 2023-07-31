6052ab21a6106365b451788cc9bbc87f79ffdaa2
mattfwilson <mattfwilson@gmail.com>
2023-07-30 23:24:10 -0400

    Debugging

--- 2021/day3/power_pt1.py
+++ 2021/day3/power_pt1.py
@@ -25,25 +25,23 @@ while counter < len(inputs[0]):
     
     if zeroes > ones:
         gamma_lst.append('0')
+        epsilon_lst.append('1')
     else:
         gamma_lst.append('1')
+        epsilon_lst.append('0')
     counter += 1
 
 gamma_binary = ''.join(gamma_lst)
-print(f'Gamma rate: {gamma_binary}')
-
-for char in gamma_binary:
-    if char == '0':
-        epsilon_lst.append('1')
-    else:
-        epsilon_lst.append('0')
+print(f'Gamma binary: {gamma_binary} | Length: {len(gamma_binary)} {type(gamma_binary)}')
 
 epsilon_binary = ''.join(epsilon_lst)
-print(f'Epsilon rate: {epsilon_binary}')
+print(f'Epsilon binary: {epsilon_binary} | Length: {len(epsilon_binary)} {type(epsilon_binary)}')
+
 gamma_decimal = to_decimal(gamma_binary)
 epsilon_decimal = to_decimal(epsilon_binary)
-print(gamma_decimal)
-print(epsilon_decimal)
+print(f'Gamma rate: {gamma_decimal}')
+print(f'Epsilon rate: {epsilon_decimal}')
 
 power = gamma_decimal * epsilon_decimal
-print(power)
+print(f'Power: {power}')
+
