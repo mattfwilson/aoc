149f99f750ade39a09a29c780dd1872926201f07
mattfwilson <mattfwilson@gmail.com>
2023-08-02 00:20:08 -0400

    First attempt at calculating oxygen rating

--- 2021/day3/power_pt1.py
+++ 2021/day3/power_pt1.py
@@ -21,3 +21,29 @@ while index < len(inputs[0]):
         digits.append(binary[index])
     zeroes = digits.count('0')
     ones = digits.count('1')
+    print(f'Zeroes: {zeroes} | Ones: {ones}')
+    
+    if zeroes > ones:
+        gamma_lst.append('0')
+        epsilon_lst.append('1')
+    else:
+        gamma_lst.append('1')
+        epsilon_lst.append('0')
+    digits.clear()
+    index += 1
+
+gamma_binary = ''.join(gamma_lst)
+print(f'Gamma binary: {gamma_binary} | Length: {len(gamma_binary)} {type(gamma_binary)}')
+
+epsilon_binary = ''.join(epsilon_lst)
+print(f'Epsilon binary: {epsilon_binary} | Length: {len(epsilon_binary)} {type(epsilon_binary)}')
+
+gamma_decimal = to_decimal(gamma_binary)
+print(f'Gamma rate: {gamma_decimal}')
+
+epsilon_decimal = to_decimal(epsilon_binary)
+print(f'Epsilon rate: {epsilon_decimal}')
+
+power = gamma_decimal * epsilon_decimal
+print(f'Power: {power}')
+
