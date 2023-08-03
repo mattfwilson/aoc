cfea6ef5a6a6e8e2cddbdb7e4022cea8e1f8a80e
mattfwilson <mattfwilson@gmail.com>
2023-08-03 01:09:56 -0400

    Updated to use dedicated function for calculating oxygen rating

--- 2021/day3/life_support.py
+++ 2021/day3/life_support.py
@@ -5,22 +5,15 @@ index = 0
 
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 
-while index < len(inputs[0]):
-    for binary in inputs:
-        digits.append(binary[index])
-    zeroes = digits.count('0')
-    ones = digits.count('1')
+def oxygen_rating(inputs, index):
+    for bit in inputs[index]:
+        oxygen_lst.append(bit)
+    most_common = max(oxygen_lst)
+    ones = oxygen_lst.count('0')
+    zeroes = oxygen_lst.count('1')
+    index += 1
+    return most_common, ones, zeroes
 
-    greater_val = 0
-    if zeroes > ones:
-        greater_val = 0
-    else:
-        greater_val = 1
-
-    for binary in inputs:
-        if binary[index] == greater_val:
-            oxygen_lst.append(binary[index])
-
-print(len(oxygen_lst))
-print(oxygen_lst)
-            
+#while index < len(inputs[0]):
+res = oxygen_rating(inputs, index)
+print(res)
