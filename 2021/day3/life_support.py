973ea8869efa9a942fe15440b1e9ef4537faa5b8
mattfwilson <mattfwilson@gmail.com>
2023-08-06 01:30:52 -0400

    While for main increment of indicies

--- 2021/day3/life_support.py
+++ 2021/day3/life_support.py
@@ -1,8 +1,5 @@
-digits = []
-oxygen_lst = []
-co2_lst = []
-res_lst = []
 keep_lst = []
+bit_lst = []
 index = 0
 
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
@@ -10,17 +7,17 @@ inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 def oxygen_rating(inputs, index):
     for binary in inputs:
         for bit in binary:
-            oxygen_lst.append(bit)
-        most_common = max(oxygen_lst)
-#        ones = oxygen_lst.count('0')
-#        zeroes = oxygen_lst.count('1')
+            bit_lst.append(bit)
+        most_common = max(bit_lst)
     for binary in inputs:
         if binary[index] == most_common:
             keep_lst.append(binary)
     return most_common
+while index <= 3:
+    res = oxygen_rating(inputs, index)
+    print(res)
+    index += 1
 
-res_lst.append(oxygen_rating(inputs, index))
 print(keep_lst)
-print(f'Len keep_lst: {len(keep_lst)}')
+print(f'Len oxygen_lst: {len(keep_lst)}')
 print(f'Len inputs: {len(inputs)}')
-index += 1
