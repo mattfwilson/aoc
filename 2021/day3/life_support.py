d7b71010aae3aeeaa6082c40ac7731c1659cf128
mattfwilson <mattfwilson@gmail.com>
2023-08-06 16:28:04 -0400

    Function counting most common currently but not incrementing index properly

--- 2021/day3/life_support.py
+++ 2021/day3/life_support.py
@@ -4,20 +4,19 @@ index = 0
 
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 
-def oxygen_rating(inputs, index):
+def oxygen_rating(inputs, bit_lst, keep_lst, index):
     for binary in inputs:
-        for bit in binary:
-            bit_lst.append(bit)
-        most_common = max(bit_lst)
+        bit_lst.append(binary[index])
+    most_common = max(bit_lst)
+
     for binary in inputs:
         if binary[index] == most_common:
             keep_lst.append(binary)
-    return most_common
-while index <= 3:
-    res = oxygen_rating(inputs, index)
-    print(res)
-    index += 1
+    return keep_lst
 
-print(keep_lst)
-print(f'Len oxygen_lst: {len(keep_lst)}')
-print(f'Len inputs: {len(inputs)}')
+while index <= len(inputs[0]) - 1:
+    res = oxygen_rating(inputs, bit_lst, keep_lst, index)
+    print(f'keep_lst: {res}')
+    print(f'Len oxygen_lst: {len(keep_lst)}')
+    print(f'Len inputs: {len(inputs)}')
+    index += 1
