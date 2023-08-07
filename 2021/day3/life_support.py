3efcc4f3423a67e7de2f69c387c3c0af44bfb5c8
mattfwilson <mattfwilson@gmail.com>
2023-08-06 23:53:41 -0400

    keep_lst totaling most common counts

--- 2021/day3/life_support.py
+++ 2021/day3/life_support.py
@@ -1,22 +1,21 @@
-keep_lst = []
+inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
+keep_lst = inputs.copy()
 bit_lst = []
 index = 0
-
-inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 
 def oxygen_rating(inputs, bit_lst, keep_lst, index):
     for binary in inputs:
         bit_lst.append(binary[index])
     most_common = max(bit_lst)
+    least_common = min(bit_lst)
 
     for binary in inputs:
         if binary[index] == most_common:
             keep_lst.append(binary)
-    return keep_lst
+    return most_common, keep_lst
 
 while index <= len(inputs[0]) - 1:
     res = oxygen_rating(inputs, bit_lst, keep_lst, index)
-    print(f'keep_lst: {res}')
-    print(f'Len oxygen_lst: {len(keep_lst)}')
-    print(f'Len inputs: {len(inputs)}')
+    print(f'most common: {res[0]} - keep_lst total: {len(res[1])}')
+    keep_lst.clear()
     index += 1
