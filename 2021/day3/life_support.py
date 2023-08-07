b53f900aeba104df76d0a8fa2280b1dd1b12d706
mattfwilson <mattfwilson@gmail.com>
2023-08-07 02:24:12 -0400

    Renamed lists and variables to represent themselves better + updated main conditional to calculate counts

--- 2021/day3/life_support.py
+++ 2021/day3/life_support.py
@@ -1,9 +1,10 @@
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
-keep_lst = inputs.copy()
+oxygen = inputs.copy()
+scrubber = inputs.copy()
 bit_lst = []
 index = 0
 
-def oxygen_rating(inputs, bit_lst, keep_lst, index):
+def calc_ratings(inputs, bit_lst, oxygen, scrubber, index):
     for binary in inputs:
         bit_lst.append(binary[index])
     most_common = max(bit_lst)
@@ -11,11 +12,14 @@ def oxygen_rating(inputs, bit_lst, keep_lst, index):
 
     for binary in inputs:
         if binary[index] == most_common:
-            keep_lst.append(binary)
-    return most_common, keep_lst
+            oxygen.append(binary)
+        elif binary[index] == least_common:
+            scrubber.append(binary)
+    return most_common, oxygen, scrubber
 
 while index <= len(inputs[0]) - 1:
-    res = oxygen_rating(inputs, bit_lst, keep_lst, index)
-    print(f'most common: {res[0]} - keep_lst total: {len(res[1])}')
-    keep_lst.clear()
+    res = calc_ratings(inputs, bit_lst, oxygen, scrubber, index)
+    print(f'most common: {res[0]} - oxygen total: {len(res[1])} - scrubber total: {len(res[2])}')
+    oxygen.clear()
+    scrubber.clear()
     index += 1
