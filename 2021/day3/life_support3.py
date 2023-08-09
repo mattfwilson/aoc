91e85e3523fa63b0ee4679fa9789f567f41ab3e1
mattfwilson <mattfwilson@gmail.com>
2023-08-09 01:29:23 -0400

    Cleaned up functions so that while loop runs withour error

--- 2021/day3/life_support3.py
+++ 2021/day3/life_support3.py
@@ -3,16 +3,10 @@ oxygen = inputs.copy()
 scrubber = inputs.copy()
 bit_lst = []
 index = 0
+most_common = '0'
+least_common = '0'
 
-def calc_ratings(inputs, bit_lst, oxygen, scrubber, index):
-    for binary in inputs:
-        if binary[index] == most_common:
-            oxygen.append(binary)
-        elif binary[index] == least_common:
-            scrubber.append(binary)
-    return most_common, least_common, oxygen, scrubber
-
-def calc_min_max(oxygen, scrubber, index):
+def calc_most_common(oxygen, scrubber, bit_lst, index):
     for binary in inputs:
         bit_lst.append(binary[index])
     most_common = max(bit_lst)
@@ -24,18 +18,19 @@ def calc_oxygen(keep_lst, index):
         for binary in keep_lst:
             if binary[index] == 1:
                 oxygen.append(binary)
+    return keep_lst
 
 def calc_scrubber(keep_lst, index):
     if most_common == least_common:
         for binary in keep_lst:
             if binary[index] == 1:
                 scrubber.append(binary)
+    return keep_lst
 
 while index <= len(inputs[0]) - 1:
-    calc_most_common(oxygen, scrubber, index)
+    calc_most_common(oxygen, scrubber, bit_lst, index)
     oyxgen_res = calc_oxygen(oxygen, index)
     scrubber_res = calc_scrubber(scrubber, index)
-    print()
-#    oxygen.clear()
-#    scrubber.clear()
     index += 1
+
+print(f'Reached end of script')
