6293530b2fcd328c928b05c26d67b48b8ea5a54b
mattfwilson <mattfwilson@gmail.com>
2023-08-10 00:10:36 -0400

    Debugging why functions are not appending valid numbers

--- 2021/day3/life_support3.py
+++ 2021/day3/life_support3.py
@@ -18,19 +18,19 @@ def calc_oxygen(keep_lst, index):
         for binary in keep_lst:
             if binary[index] == 1:
                 oxygen.append(binary)
-    return keep_lst
 
 def calc_scrubber(keep_lst, index):
     if most_common == least_common:
         for binary in keep_lst:
             if binary[index] == 1:
                 scrubber.append(binary)
-    return keep_lst
 
 while index <= len(inputs[0]) - 1:
     calc_most_common(oxygen, scrubber, bit_lst, index)
-    oyxgen_res = calc_oxygen(oxygen, index)
+    oxygen_res = calc_oxygen(oxygen, index)
     scrubber_res = calc_scrubber(scrubber, index)
+    print(f'oxygen_res: {len(oxygen)}')
+    print(f'scrubber_res: {len(scrubber)}')
     index += 1
 
 print(f'Reached end of script')
