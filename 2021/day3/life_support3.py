0862df7ac47195960cad6e5dca754b778ead58b6
mattfwilson <mattfwilson@gmail.com>
2023-08-11 03:30:13 -0400

    Using sets and count inside of max()

--- 2021/day3/life_support3.py
+++ 2021/day3/life_support3.py
@@ -3,34 +3,29 @@ oxygen = inputs.copy()
 scrubber = inputs.copy()
 bit_lst = []
 index = 0
-most_common = '0'
-least_common = '0'
+most_common = 0
+least_common = 0
 
-def calc_most_common(oxygen, scrubber, bit_lst, index):
+def calc_most_common(inputs, scrubber, bit_lst, index):
     for binary in inputs:
-        bit_lst.append(binary[index])
-    most_common = max(bit_lst)
-    least_common = min(bit_lst)
-    return most_common, least_common
+        bit_lst.append(int(binary[index]))
+    most = max(set(bit_lst), key=bit_lst.count)
+    return most
 
-def calc_oxygen(keep_lst, index):
-    if most_common == least_common:
-        for binary in keep_lst:
-            if binary[index] == 1:
-                oxygen.append(binary)
-
-def calc_scrubber(keep_lst, index):
-    if most_common == least_common:
-        for binary in keep_lst:
-            if binary[index] == 1:
-                scrubber.append(binary)
+def calc_ratings(keep_lst, most_common, least_common, index):
+    for num in keep_lst:
+        if num[index] == most_common:
+            oxygen.append(num)
+        elif num[index] == least_common:
+            scrubber.append(num)
+    return oxygen, scrubber
 
 while index <= len(inputs[0]) - 1:
-    calc_most_common(oxygen, scrubber, bit_lst, index)
-    oxygen_res = calc_oxygen(oxygen, index)
-    scrubber_res = calc_scrubber(scrubber, index)
-    print(f'oxygen_res: {len(oxygen)}')
-    print(f'scrubber_res: {len(scrubber)}')
+    most_common = calc_most_common(oxygen, scrubber, bit_lst, index)
+    ratings = calc_ratings(oxygen, most_common, least_common, index)
+    print(f'most common: {most_common}')
+    print(f'ratings: {len(ratings[0])} / {len(ratings[1])}')
+    bit_lst.clear()
     index += 1
 
 print(f'Reached end of script')
