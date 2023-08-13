5663ddb91cf93e6c6bc2c249723234d8a7562021
mattfwilson <mattfwilson@gmail.com>
2023-08-13 02:47:32 -0400

    Cleaned up necessary parameters and arguments

--- 2021/day3/life_support3.py
+++ 2021/day3/life_support3.py
@@ -1,14 +1,14 @@
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
-oxygen = inputs.copy()
-scrubber = inputs.copy()
+oxygen = []
+scrubber = []
 bit_lst = []
 index = 0
 most_common = 0
 least_common = 0
 
-def calc_most_common(inputs, scrubber, bit_lst, index):
+def calc_most_common(inputs, index):
     for binary in inputs:
-        bit_lst.append(int(binary[index]))
+        bit_lst.append(binary[index])
     most_common = max(set(bit_lst), key=bit_lst.count)
     return most_common
 
@@ -21,8 +21,8 @@ def calc_ratings(keep_lst, most_common, least_common, index):
     return oxygen, scrubber
 
 while index <= len(inputs[0]) - 1:
-    most_common = calc_most_common(oxygen, scrubber, bit_lst, index)
-    ratings = calc_ratings(oxygen, most_common, least_common, index)
+    most_common = calc_most_common(inputs, index)
+    ratings = calc_ratings(inputs, most_common, least_common, index)
     print(f'most common: {most_common}')
     print(f'ratings: {len(ratings[0])} / {len(ratings[1])}')
     bit_lst.clear()
