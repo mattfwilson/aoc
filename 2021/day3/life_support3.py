a0cdc794f7e101ef0be1e570f493484794aa1b66
mattfwilson <mattfwilson@gmail.com>
2023-11-15 03:06:10 -0500

    Recreated separate functions for scrubber and oxygen calculations

--- 2021/day3/life_support3.py
+++ 2021/day3/life_support3.py
@@ -12,13 +12,19 @@ def calc_most_common(inputs, index):
     most_common = max(set(bit_lst), key=bit_lst.count)
     return most_common
 
+def calc_scrubber(inputs):
+    pass
+
+def calc_oxygen(inputs):
+    pass
+
 def calc_ratings(keep_lst, most_common, least_common, index):
     for num in keep_lst:
         if num[index] == most_common:
             oxygen.append(num)
         else:
             scrubber.append(num)
-    return oxygen, scrubber
+    return oxygen, scrubbeh
 
 print(type(inputs))
 for i in inputs:
@@ -26,7 +32,9 @@ for i in inputs:
 
 while index <= len(inputs[0]) - 1:
     most_common = calc_most_common(inputs, index)
-    ratings = calc_ratings(inputs, most_common, least_common, index)
+#    ratings = calc_ratings(inputs, most_common, least_common, index)
+    calc_scrubber(inputs)
+    calc_oxygen(inputs)
     print(f'most common: {most_common}')
     print(f'ratings: {len(ratings[0])} / {len(ratings[1])}')
     bit_lst.clear()
