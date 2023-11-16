33e44aeff65c2ae1d8eae9c4eba11f53ece3f440
mattfwilson <mattfwilson@gmail.com>
2023-11-16 02:22:51 -0500

    Reconfiguring while look to be inside calc function

--- 2021/day3/life_support3.py
+++ 2021/day3/life_support3.py
@@ -12,25 +12,22 @@ def calc_most_common(inputs, index):
     most_common = max(set(bit_lst), key=bit_lst.count)
     return most_common
 
-def calc_ratings(keep_lst, most_common, least_common, index, oxygen, scrubber):
-    for num in keep_lst:
-        if num[index] == most_common:
-            oxygen.append(num)
-        else:
-            scrubber.append(num)
+def calc_oxygen(inputs, oxygen, scrubber, most_common, index):
+    while len(oxygen) > 1:
+        for num in inputs:
+            if num[index] == most_common:
+                oxygen.append(num)
+            else:
+                scrubber.append(num)
+        index += 1
     return oxygen, scrubber
 
-#print(type(inputs))
-#for i in inputs:
-#    print(f'{i} - {type(i)}')
+most_common = calc_most_common(inputs, index)
+oxygen = calc_oxygen(inputs, oxygen, scrubber, most_common, index)
 
-while index <= len(inputs[0]) - 1:
-    most_common = calc_most_common(inputs, index)
-    ratings = calc_ratings(inputs, most_common, least_common, index, oxygen, scrubber)
-    print(f'most common: {most_common}')
-    print(f'oxygen: {oxygen} - len: {len(oxygen)}')
-    print(f'scrubber: {scrubber} - {len(scrubber)}')
-    bit_lst.clear()
-    index += 1
+print(f'most common: {most_common}')
+print(f'oxygen: {oxygen} - len: {len(oxygen)}')
+print(f'scrubber: {scrubber} - {len(scrubber)}')
 
+print(oxygen)
 print(f'Reached end of script')
