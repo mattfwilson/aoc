08f9303244e68b19eb27d71f235da029615e50a5
mattfwilson <mattfwilson@gmail.com>
2023-12-06 00:29:00 -0500

    Lost on day 1 ugh

--- 2023/day1/calib_pt2f.py
+++ 2023/day1/calib_pt2f.py
@@ -5,9 +5,13 @@ output_lst = []
 str_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 str_digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-words = ['twoclsk5jd4fzerone23threeight']
+words = ['ninel2d4oneight', 'fiveightks2d766kdzero', 'twoclsk5jd4fzerone23threeight']
 
-for line in words:
-    for digit in str_digits:
-        output_lst.append(line.find(digit))
-    print(output_lst)
+for word in words:
+    temp_lst = []
+    for index, digit in enumerate(str_digits):
+        if digit in word:
+            temp_lst.append(word.replace(digit, str(index)))
+
+    print(temp_lst)
+
