056e9a69a37409b536e7519a813e4d74c80ebc1d
mattfwilson <mattfwilson@gmail.com>
2023-12-06 02:44:32 -0500

    Getting closer on solution to finding end of string digit

--- 2023/day1/calib_pt2f.py
+++ 2023/day1/calib_pt2f.py
@@ -5,13 +5,36 @@ output_lst = []
 str_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 str_digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-words = ['ninel2d4oneight', 'fiveightks2d766kdzero', 'twoclsk5jd4fzerone23threeight']
+words = ['2twoclsk5jd4fzerone23threeight']
+temp_lst = []
+result = []
 
 for word in words:
-    temp_lst = []
     for index, digit in enumerate(str_digits):
         if digit in word:
             temp_lst.append(word.replace(digit, str(index)))
 
-    print(temp_lst)
+for i in temp_lst:
+    print(i)
 
+for string in temp_lst:
+    index = 0
+    while len(result) < 1:
+        if string[index].isdigit():
+            print(f'{string[index]} is a digit')
+            result.append(int(string[index]))
+        else:
+            print('not a digit')
+        index += 1
+
+for string in temp_lst:
+    neg_index = -1
+    while len(result) < 2:
+        if string[neg_index].isdigit():
+            print(f'{string[neg_index]} is a digit')
+            result.append(int(string[neg_index]))
+        else:
+            print('not a digit')
+        neg_index += -1
+
+print(result)
