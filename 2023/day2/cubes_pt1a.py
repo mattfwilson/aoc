92d22b429d5a673ee648219d022aec43ae5d8cbd
mattfwilson <mattfwilson@gmail.com>
2023-12-08 14:48:14 -0500

    List comprehension for separating individual game lines

--- 2023/day2/cubes_pt1a.py
+++ 2023/day2/cubes_pt1a.py
@@ -1,20 +1,11 @@
-inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
-input_lst = [line for line in inputs]
+games = []
+inputs = [games.append(line) for line in open('inputs.txt').read().strip().split('\n')]
 
 game_index = 0
 red = 0
 green = 0
 blue = 0
 
-for line in input_lst:
-    temp_lst = []
-    temp_lst2 = []
-
-    temp_lst.append(line)
-    print(temp_lst)
-    for line in temp_lst:
-        line = line.strip('Game: ')
+for line in games:
+    line = line.strip('Game: ')
     print(line)
-
-
-
