0b20b52cbda159f25eea6aa381c40ecb8adcd827
mattfwilson <mattfwilson@gmail.com>
2023-12-08 14:38:11 -0500

    Initial pass on strippip index for each game played

--- 2023/day2/cubes_pt1a.py
+++ 2023/day2/cubes_pt1a.py
@@ -1,2 +1,20 @@
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
-inputs_lst = [line for line in inputs]
+input_lst = [line for line in inputs]
+
+game_index = 0
+red = 0
+green = 0
+blue = 0
+
+for line in input_lst:
+    temp_lst = []
+    temp_lst2 = []
+
+    temp_lst.append(line)
+    print(temp_lst)
+    for line in temp_lst:
+        line = line.strip('Game: ')
+    print(line)
+
+
+
