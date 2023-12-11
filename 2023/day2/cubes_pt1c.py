903fd9dc9d837afc22841e8033d09533437cd074
mattfwilson <mattfwilson@gmail.com>
2023-12-11 02:32:26 -0500

    Breaking down nested list comprehension to understand it

--- 2023/day2/cubes_pt1c.py
+++ 2023/day2/cubes_pt1c.py
@@ -7,8 +7,14 @@ green = 13
 blue = 14
 
 for line in inputs:
-    game_id, rounds = line.strip().split(': ')
+    game_id, draws = line.strip().split(': ')
     game_id = int(game_id.split(' ')[1])
+    games = draws.split('; ')
 
-print(f'{game_id} - {type(game_id)}')
-print(f'{rounds} - {type(rounds)}')
+    #draws = [[item.split(' ') for item in draw.split(', ')] for draw in draws.split('; ')]
+
+    for game in games:
+        game.split('; ')
+        for item in game.split(', '):
+            item.split(' ')
+        print(game)
