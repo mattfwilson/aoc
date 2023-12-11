6483bf35cdd18f0c27de2ccc700719a8462ff3db
mattfwilson <mattfwilson@gmail.com>
2023-12-11 17:11:32 -0500

    First attempt at check for exceeding limit off of totals dict

--- 2023/day2/cubes_pt1c.py
+++ 2023/day2/cubes_pt1c.py
@@ -1,15 +1,17 @@
-import re
+from itertools import chain
 
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
-
-red = 12
-green = 13
-blue = 14
+totals = {'red': 12, 'green': 13, 'blue': 14}
 
 for line in inputs:
     game_id, draws = line.strip().split(': ')
     game_id = int(game_id.split(' ')[1])
-    games = draws.split('; ')
     draws = [[item.split(' ') for item in draw.split(', ')] for draw in draws.split('; ')]
-    print(f'Game {game_id} - {draws}\n')
+    merged_draws = [item for sublist in draws for item in sublist]
+    print(f'Game {game_id} - {merged_draws}\n')
+    for draw in merged_draws:
+        if 'red' in draw[1]:
+            if int(draw[0]) > totals['red']:
+                print(f'Game id {game_id}')
+
    
