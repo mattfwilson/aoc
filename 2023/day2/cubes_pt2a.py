1b8c0577f8747957c68895705537bf7b11ca0a5b
mattfwilson <mattfwilson@gmail.com>
2023-12-13 15:59:33 -0500

    Solution for day 2 part 2

--- 2023/day2/cubes_pt2a.py
+++ 2023/day2/cubes_pt2a.py
@@ -1,13 +1,12 @@
+import math
+
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 totals = {'red': 12, 'green': 13, 'blue': 14}
 ids = []
 red_max = 0
 green_max = 0
 blue_max = 0
-
-reds = []
-greens = []
-blues = []
+summed = 0
 
 for line in inputs:
     game_id, draws = line.strip().split(': ')
@@ -15,18 +14,20 @@ for line in inputs:
     ids.append(game_id)
     draws = [[item.split(' ') for item in draw.split(', ')] for draw in draws.split('; ')]
     merged_draws = [item for sublist in draws for item in sublist]
-    print(merged_draws)
-    for draw in merged_draws:
-        if 'red' in draw[1] and int(draw[0]) > red_max:
-            red_max = int(draw[0])
-            reds.append(red_max)
-        if 'green' in draw[1] and int(draw[0]) > green_max:
-            green_max = int(draw[0])
-            greens.append(green_max)
-        if 'blue' in draw[1] and int(draw[0]) > blue_max:
-            blue_max = int(draw[0])
-            blues.append(blue_max)
+    print(f'MERGED DRAWS: {merged_draws}')
 
-print(reds)
-print(greens)
-print(blues)
+    for draw in merged_draws:
+        print(f'DRAW: {draw}')
+        if draw[1] == 'red' and int(draw[0]) > red_max:
+            red_max = int(draw[0])
+        if draw[1] == 'green' and int(draw[0]) > green_max:
+            green_max = int(draw[0])
+        if draw[1]  == 'blue' and int(draw[0]) > blue_max:
+            blue_max = int(draw[0])
+        print(f'reds: {red_max}, greens: {green_max}, blues: {blue_max}')
+    summed += red_max * green_max * blue_max
+    red_max = 0
+    green_max = 0
+    blue_max = 0
+
+print(f'Sum of powers: {summed}')
