96db46f6e851213831d61d76a6d8f69573123074
mattfwilson <mattfwilson@gmail.com>
2023-12-13 03:20:42 -0500

    Finding max (possible) draws

--- 2023/day2/cubes_pt2a.py
+++ 2023/day2/cubes_pt2a.py
@@ -1,10 +1,13 @@
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 totals = {'red': 12, 'green': 13, 'blue': 14}
 ids = []
+red_max = 0
+green_max = 0
+blue_max = 0
+
 reds = []
 greens = []
 blues = []
-mins = []
 
 for line in inputs:
     game_id, draws = line.strip().split(': ')
@@ -12,19 +15,18 @@ for line in inputs:
     ids.append(game_id)
     draws = [[item.split(' ') for item in draw.split(', ')] for draw in draws.split('; ')]
     merged_draws = [item for sublist in draws for item in sublist]
+    print(merged_draws)
     for draw in merged_draws:
-        if 'red' in draw[1]:
-            reds.append(int(draw[0]))
-        if 'green' in draw[1]:
-            greens.append(int(draw[0]))
-        if 'blue' in draw[1]:
-            blues.append(int(draw[0]))
+        if 'red' in draw[1] and int(draw[0]) > red_max:
+            red_max = int(draw[0])
+            reds.append(red_max)
+        if 'green' in draw[1] and int(draw[0]) > green_max:
+            green_max = int(draw[0])
+            greens.append(green_max)
+        if 'blue' in draw[1] and int(draw[0]) > blue_max:
+            blue_max = int(draw[0])
+            blues.append(blue_max)
 
-        min_reds = min(reds)
-        min_greens = min(greens)
-        min_blues = min(blues)
-
-    print(type(mins))
-print(type(mins))
-
-print(sum(mins))
+print(reds)
+print(greens)
+print(blues)
