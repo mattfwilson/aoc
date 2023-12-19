47e1b44a2c7b20cc9c0e876e270d8036c703d5e9
mattfwilson <mattfwilson@gmail.com>
2023-12-19 00:26:27 -0500

    Removed unnecessary function to check for symbols

--- 2023/day3/scheme_pt1b.py
+++ 2023/day3/scheme_pt1b.py
@@ -2,13 +2,9 @@ grid = []
 symbols = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 
-def check_for_symbols(line, char):
-    if char != '.':
-        print(f'{char} - {line}')
-
-for index, line in enumerate(grid):
-    for char in line:
-        check_for_symbols(line, char)
+for line_i, line in enumerate(grid):
+    for char_i, char in enumerate(line):
+        print(f'line index: {char_i}, char index: {char_i}')
         if not char.isnumeric() and char != '.' and char not in symbols:
             symbols.append(char)
 
