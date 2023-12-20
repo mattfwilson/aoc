a25be3be46be352e356c858e8c09890ba789c050
mattfwilson <mattfwilson@gmail.com>
2023-12-20 01:31:54 -0500

    Printing all digits that are not symbols

--- 2023/day3/scheme_pt1b.py
+++ 2023/day3/scheme_pt1b.py
@@ -1,11 +1,15 @@
 grid = []
 symbols = []
+targets = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 
 for line_i, line in enumerate(grid):
     for char_i, char in enumerate(line):
-        print(f'line index: {char_i}, char index: {char_i}')
+        print(f'line index: {line_i}, char index: {char_i}')
         if not char.isnumeric() and char != '.' and char not in symbols:
             symbols.append(char)
+        elif char != '.' and char not in symbols:
+            targets.append(char)
 
 print(symbols)
+print(targets)
