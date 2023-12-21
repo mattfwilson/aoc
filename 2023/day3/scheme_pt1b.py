82fb713c059551c77eed5ad1e6ee64d416c0e82a
mattfwilson <mattfwilson@gmail.com>
2023-12-21 01:06:53 -0500

    Char and char index being printed together from enums

--- 2023/day3/scheme_pt1b.py
+++ 2023/day3/scheme_pt1b.py
@@ -3,13 +3,16 @@ symbols = []
 targets = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 
+def check_for_symbols(line, char):
+    return line, char
+
 for line_i, line in enumerate(grid):
     for char_i, char in enumerate(line):
         print(f'line index: {line_i}, char index: {char_i}')
         if not char.isnumeric() and char != '.' and char not in symbols:
             symbols.append(char)
         elif char != '.' and char not in symbols:
-            targets.append(char)
+            targets.append((char_i, char))
 
 print(symbols)
 print(targets)
