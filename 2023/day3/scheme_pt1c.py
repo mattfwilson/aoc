c9c83d1979c37962f75d8ebbd2345d1fb06f6a68
mattfwilson <mattfwilson@gmail.com>
2023-12-31 21:35:33 -0500

    Enumerating lines to show indices

--- 2023/day3/scheme_pt1c.py
+++ 2023/day3/scheme_pt1c.py
@@ -4,18 +4,23 @@ grid_lines = []
 my_list = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 
-for line in grid_lines:
-    for char in line:
-        if not char.isnumeric() and char != '.' and char not in symbols:
-            symbols.append(char)
+for line in grid:
+    grid_lines.append(line)
 
-for i, value in enumerate(inputs):
-    index = i
+for i in enumerate(grid_lines):
+    value = i
     my_list.append(value)
-    print(f'Normal index: {index}')
-    neg_index = i - 1
-    print(f'Negative index: {neg_index}')
-    pos_index = i + 1
-    print(f'Positive index: {pos_index}')
 
-print(symbols)
+index_choice = 0
+index_after = index_choice + 1
+index_before = index_choice - 1
+
+for line in my_list:
+    index_choice += 1
+    print(line)
+#    if index_choice < 0:
+#        print(f'current: {index_choice}, after: {index_after}')
+#    elif index_choice == len(my_list):
+#        print(f'before: {index_before}, current: {index_choice}')
+#    else:
+#        print(f'before: {index_before}, current: {index_choice}, after: {index_after}')
