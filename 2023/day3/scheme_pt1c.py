9ae1093156b550e00bc9002286090235d738409e
mattfwilson <mattfwilson@gmail.com>
2024-01-01 15:20:07 -0500

    Trying to simplify enumeration and loops

--- 2023/day3/scheme_pt1c.py
+++ 2023/day3/scheme_pt1c.py
@@ -3,25 +3,22 @@ symbols = []
 grid_lines = []
 my_list = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
+index = 0
+index_after = index + 1
+index_before = index - 1
 
 for line in grid:
     grid_lines.append(line)
 
 for i in enumerate(grid_lines):
-    value = i
-    my_list.append(value)
-
-index = 0
-index_after = index + 1
-index_before = index - 1
-
+    val = i
+    my_list.append(val)
 
 for index in my_list:
-    if index == 0:
+    if index[0] == 0:
         print(f'current: {my_list[index]}, after: {my_list[index_after]}')
-    elif index == len(my_list):
+    elif index[0] == len(my_list):
         print(f'before: {my_list[index_before]}, current: {my_list[index]}')
     else:
         print(f'before: {my_list[index_before]}, current: {my_list[index]}, after: {my_list[index_after]}')
 
-print(len(my_list))
