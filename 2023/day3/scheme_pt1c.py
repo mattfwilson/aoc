ac80035aad37a3c12d365948b446ebac1238f22c
mattfwilson <mattfwilson@gmail.com>
2024-01-01 23:22:14 -0500

    Lines in lists within a list

--- 2023/day3/scheme_pt1c.py
+++ 2023/day3/scheme_pt1c.py
@@ -2,20 +2,20 @@ grid = []
 grid_list = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 
-index = 0
-index_after = index + 1
-index_before = index - 1
-
 grid_lst = [[index, line] for index, line in enumerate(grid)]
 
 print(grid_lst)
-print(len(grid_lst))
 
-#for index in my_list:
-#    if index[0] == 0:
-#        print(f'current: {my_list[index]}, after: {my_list[index_after]}')
-#    elif index[0] == len(my_list):
-#        print(f'before: {my_list[index_before]}, current: {my_list[index]}')
-#    else:
-#        print(f'before: {my_list[index_before]}, current: {my_list[index]}, after: {my_list[index_after]}')
+for item in grid_lst:
+    for i in item:
+        index = 0
+        index_after = index + 1
+        index_before = index - 1
+        print(i)
+        if i[0] == 0:
+            print(f'current: {grid_lst[index]}, after: {grid_lst[index_after]}')
+        elif i[0] == len(grid_lst):
+            print(f'before: {grid_lst[index_before]}, current: {grid_lst[index]}')
+        else:
+            print(f'before: {grid_lst[index_before]}, current: {grid_lst[index]}, after: {grid_lst[index_after]}')
 
