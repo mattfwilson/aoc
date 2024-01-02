faec39077346fb91d343a1643fc540ae665c53b8
mattfwilson <mattfwilson@gmail.com>
2024-01-01 19:09:55 -0500

    Properly enumerating lines from original input

--- 2023/day3/scheme_pt1c.py
+++ 2023/day3/scheme_pt1c.py
@@ -1,24 +1,21 @@
 grid = []
-symbols = []
-grid_lines = []
-my_list = []
+grid_list = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
+
 index = 0
 index_after = index + 1
 index_before = index - 1
 
-for line in grid:
-    grid_lines.append(line)
+grid_lst = [[index, line] for index, line in enumerate(grid)]
 
-for i in enumerate(grid_lines):
-    val = i
-    my_list.append(val)
+print(grid_lst)
+print(len(grid_lst))
 
-for index in my_list:
-    if index[0] == 0:
-        print(f'current: {my_list[index]}, after: {my_list[index_after]}')
-    elif index[0] == len(my_list):
-        print(f'before: {my_list[index_before]}, current: {my_list[index]}')
-    else:
-        print(f'before: {my_list[index_before]}, current: {my_list[index]}, after: {my_list[index_after]}')
+#for index in my_list:
+#    if index[0] == 0:
+#        print(f'current: {my_list[index]}, after: {my_list[index_after]}')
+#    elif index[0] == len(my_list):
+#        print(f'before: {my_list[index_before]}, current: {my_list[index]}')
+#    else:
+#        print(f'before: {my_list[index_before]}, current: {my_list[index]}, after: {my_list[index_after]}')
 
