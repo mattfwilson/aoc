d12ec68f9af0eb4245eaf8f967fb4e86ebd44fd4
mattfwilson <mattfwilson@gmail.com>
2024-01-03 00:40:42 -0500

    Cleaner debug for getting line before/after current iteration

--- 2023/day3/scheme_pt1c.py
+++ 2023/day3/scheme_pt1c.py
@@ -3,15 +3,20 @@ grid_list = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 grid_lst = [[index, line] for index, line in enumerate(grid)]
 
+print(grid_lst)
+
 for item in grid_lst:
     index = 0
-    index_after = index + 1
-    index_before = index - 1
+    index_after = int(index + 1)
+    index_before = int(index - 1)
+    count = 0
     print(item)
-    if item[0] == 0:
-        print(f'current: {grid_lst[index]}, after: {grid_lst[index_after]}')
-    elif item[0] == len(grid_lst):
-        print(f'before: {grid_lst[index_before]}, current: {grid_lst[index]}')
+    if item[count] == 0:
+        print(f'current: {grid_lst[index]}\nafter: {grid_lst[index_after]}\n')
+        count += 1
+    elif item[count] == len(grid_lst):
+        print(f'before: {grid_lst[index_before]}\ncurrent: {grid_lst[index]}\n')
+        count += 1
     else:
-        print(f'before: {grid_lst[index_before]}, current: {grid_lst[index]}, after: {grid_lst[index_after]}')
-
+        print(f'before: {grid_lst[index_before]}\ncurrent: {grid_lst[index]}\nafter: {grid_lst[index_after]}\n')
+        count += 1
