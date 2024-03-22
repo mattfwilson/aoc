1840f647998d997791e06d742e938647341bc330
mattfwilson <mattfwilson@gmail.com>
2024-03-22 00:27:58 -0400

    Tweaks

--- 2023/day3/scheme_pt1c.py
+++ 2023/day3/scheme_pt1c.py
@@ -3,8 +3,6 @@ grid_list = []
 inputs = [grid.append(line) for line in open('inputs.txt').read().split('\n')]
 grid_lst = [[index, line] for index, line in enumerate(grid)]
 
-print(grid_lst)
-
 for item in grid_lst:
     index = 0
     index_after = index + 1
