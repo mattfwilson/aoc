c1d3b369a796784a1782904c46645c5d0d90edc2
mattfwilson <mattfwilson@gmail.com>
2024-06-18 00:55:36 -0400

    Renamed function and removed old debugging

--- 2021/day3/life_support5.py
+++ 2021/day3/life_support5.py
@@ -4,13 +4,12 @@ inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 bit_lst = []
 index = 0
 
-def find_most_bits(inputs, index):
+def remove_bits(inputs, index):
     print(f'Index iteration: {index}')
 
     counter = Counter(inputs[index])
     most_common_item = counter.most_common()[0][0]
     print(most_common_item)
-    print(type(most_common_item))
 
     for line in inputs:
         if line[index] == most_common_item:
@@ -23,6 +22,6 @@ while True:
     if index > 11:
         break
     else:
-        inputs = find_most_bits(inputs, index)
+        inputs = remove_bits(inputs, index)
     index += 1
 
