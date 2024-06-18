30f83d37e9563e57c7c96d315a98c462c0a078a0
mattfwilson <mattfwilson@gmail.com>
2024-06-18 01:50:47 -0400

    Recursive function attempt

--- 2021/day3/life_support5.py
+++ 2021/day3/life_support5.py
@@ -2,26 +2,24 @@ from collections import Counter
 
 inputs = [line for line in open('inputs.txt').read().strip().split('\n')]
 bit_lst = []
-index = 0
 
-def remove_bits(inputs, index):
-    print(f'Index iteration: {index}')
-
-    counter = Counter(inputs[index])
-    most_common_item = counter.most_common()[0][0]
-    print(most_common_item)
-
-    for line in inputs:
-        if line[index] == most_common_item:
-            continue
+def remove_bits(inputs, index, end):
+    while True:
+        if index > end:
+            return inputs
         else:
-            inputs.remove(line)
-    return inputs
+            counter = Counter(inputs[index])
+            most_common_item = counter.most_common()[0][0]
+            print(f'Index iteration: {index}')
+            print(most_common_item)
 
-while True:
-    if index > 11:
-        break
-    else:
-        inputs = remove_bits(inputs, index)
-    index += 1
+            for line in inputs:
+                if line[index] == most_common_item:
+                    pass
+                else:
+                    inputs.remove(line)
+        index += 1
+        
+    return remove_bits(inputs, index + 1, end)
 
+remove_bits(inputs, 0, 11)
