1784b33f4049787e8257db49de22527c817d1b1c
mattfwilson <mattfwilson@gmail.com>
2024-12-25 00:59:07 -0500

    Fixed output lists (still incorrect count)

--- 2024/day2/reports.py
+++ 2024/day2/reports.py
@@ -1,26 +1,40 @@
 inputs = []
 int_inputs = []
-is_consecutive = []
-is_decreasing = []
+valid_diffs = []
+is_increasing = []
+res_lst = []
 
 
 with open('inputs.txt', 'r') as file:
     for line in file:
         inputs.append(line.strip().split())
 
+# convert list to ints
 for lst in inputs:
     int_inputs.append([int(num) for num in lst])
-        
+
+
+# filter differences equaling 1-3
 for lst in int_inputs:
+    if all(lst[i] - lst[i + 1] for i in range(len(lst) - 1)) > 0 and all(lst[i] - lst[i + 1] for i in range(len(lst) - 1)) < 4:
+        valid_diffs.append(lst)
+
+print(f'good_diffs: {valid_diffs}')
+print(len(valid_diffs))
+
+# filter strictly increasing
+for lst in valid_diffs:
     if all(lst[i] < lst[i + 1] for i in range(len(lst) - 1)):
-        is_consecutive.append(lst)
+        is_increasing.append(lst)
 
-print(f'increasing check: {is_consecutive}')
-print(len(is_consecutive))
+print(f'is_increasing: {is_increasing}')
+print(len(is_increasing))
 
-for lst in int_inputs:
+# filter strictly decreasing
+for lst in valid_diffs:
     if all(lst[i] > lst[i + 1] for i in range(len(lst) - 1)):
-        is_consecutive.append(lst)
+        res_lst.append(lst)
 
-print(f'decreasing check: {is_consecutive}')
-print(len(is_consecutive))
+print(f'res_lst: {res_lst}')
+print(len(res_lst))
+
