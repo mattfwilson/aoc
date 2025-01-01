2968a07d862ad16f7ada56e523a87fb28069f6bc
mattfwilson <mattfwilson@gmail.com>
2025-01-01 02:37:16 -0500

    Removed comments to declutter

--- 2024/day2/reports3.py
+++ 2024/day2/reports3.py
@@ -5,13 +5,11 @@ with open('inputs.txt', 'r') as file:
     for line in file:
         inputs.append(line.strip().split())
 
-# convert strs to ints
 for lst in inputs:
     int_inputs.append([int(num) for num in lst])
 
 print(f'int_inputs after dupe removal: {len(int_inputs)}')
 
-# remove lsts not in valid range
 for lst in int_inputs:
     valid_range = [abs(lst[i] - lst[i + 1]) for i in range(len(lst) - 1)]
 
@@ -23,7 +21,6 @@ for lst in int_inputs:
 
 print(f'int_inputs after range check: {len(int_inputs)}')
 
-# remove lsts that are not strictly increasing/decreasing
 for lst in int_inputs:
     if all(lst[i] < lst[i + 1] for i in range(len(lst) - 1)):
         pass
