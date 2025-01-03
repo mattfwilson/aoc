6c13a1a8b7171a847c9d275ab06990b15f42c948
mattfwilson <mattfwilson@gmail.com>
2025-01-03 01:41:14 -0500

    Fixed bugs

--- 2024/day2/reports3.py
+++ 2024/day2/reports3.py
@@ -8,19 +8,16 @@ with open('inputs.txt', 'r') as file:
 for lst in inputs:
     int_inputs.append([int(num) for num in lst])
 
-print(type(inputs[0][0]))
-print(type(int_inputs[0][0])
-
 print(f'int_inputs after dupe removal: {len(int_inputs)}')
 
 for lst in int_inputs:
     valid_range = [abs(lst[i] - lst[i + 1]) for i in range(len(lst) - 1)]
 
-    for j in valid_range:
-        if 0 < lst[j] < 4:
-            pass
+    for num in valid_range:
+        if 0 < num < 4:
+            continue
         else:
-            int_inputs.remove(lst)
+            valid_range.remove(num)
 
 print(f'int_inputs after range check: {len(int_inputs)}')
 
