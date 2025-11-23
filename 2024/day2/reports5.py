63f2dca6b6eba39221722ffbf2805a3b16582a81
mattfwilson <mattfwilson@gmail.com>
2025-11-23 18:00:54 -0500

    Fixes for finding correct amounts of sorted/reverse sorted reports

--- 2024/day2/reports5.py
+++ 2024/day2/reports5.py
@@ -1,6 +1,5 @@
 inputs = []
 safe_reports = []
-diffs = []
 
 with open('inputs.txt', 'r') as file:
     for line in file:
@@ -8,12 +7,15 @@ with open('inputs.txt', 'r') as file:
         inputs.append([int(n) for n in levels])
 
 for report in inputs:
-    if report == sorted(report) or report == sorted(report, reverse=True):
+    if report == sorted(report):
         diffs = [abs(report[i + 1] - report[i]) for i in range(len(report) - 1)]
 
-    for num in diffs:
-        if num >= 1 and num <= 3:
-            safe_reports.append(report)
+    if report == sorted(report, reverse=True):
+        diffs = [abs(report[i] - report[i + 1]) for i in range(len(report) - 1)]
+        
+        for num in diffs:
+            if num >= 1 and num <= 3:
+                safe_reports.append(report)
 
                 
 print(safe_reports)
